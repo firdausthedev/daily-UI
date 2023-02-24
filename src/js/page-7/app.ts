50ee61461731ca1d63d5c8f76f5f3a32663111b1
firdaus <firdausthedev@gmail.com>
2023-02-24 11:14:28 +0800

    page 7 step 1-3 js logic

---
+++ src/js/page-7/app.ts
@@ -0,0 +1,179 @@
+// Constants
+const FIELD_REQUIRED = "This field is required";
+const EMAIL_INVALID_FORMAT = "Email if not formatted correctly";
+const PLAN_YEARLY = "2 months free";
+
+// inputs
+const name_input = document.getElementById("name")! as HTMLInputElement;
+const email_input = document.getElementById("email")! as HTMLInputElement;
+const phone_input = document.getElementById("phone")! as HTMLInputElement;
+const price_toggle = document.getElementById(
+  "plan-toggle",
+)! as HTMLInputElement;
+const online_cb = document.getElementById("addons-online")! as HTMLInputElement;
+const larger_cb = document.getElementById(
+  "addons-larger-storage",
+)! as HTMLInputElement;
+const custom_cb = document.getElementById("addons-custom")! as HTMLInputElement;
+
+// helpers
+const name_helpers = document.querySelector(
+  ".helper-name",
+)! as HTMLParagraphElement;
+const email_helpers = document.querySelector(
+  ".helper-email",
+)! as HTMLParagraphElement;
+const phone_helpers = document.querySelector(
+  ".helper-phone",
+)! as HTMLParagraphElement;
+
+// buttons
+const form_1_next = document.querySelector(
+  ".form-1 button",
+)! as HTMLButtonElement;
+const form_2_prev = document.querySelector(
+  ".form-2 button.prev",
+)! as HTMLButtonElement;
+const form_2_next = document.querySelector(
+  ".form-2 button.next",
+)! as HTMLButtonElement;
+const form_3_prev = document.querySelector(
+  ".form-3 button.prev",
+)! as HTMLButtonElement;
+const form_3_next = document.querySelector(
+  ".form-3 button.next",
+)! as HTMLButtonElement;
+
+const show_form = (currentForm: number, nextForm: number) => {
+  document.querySelector(`.form-${currentForm}`)!.classList.add("hidden");
+  document.querySelector(`.form-${nextForm}`)!.classList.remove("hidden");
+};
+
+// nav functions
+const set_active_nav = (num: number) => {
+  document.querySelectorAll(".nav").forEach(item => {
+    item.classList.remove("active-nav");
+  });
+
+  document.querySelector(`.nav-${num}`)!.classList.add("active-nav");
+};
+
+// form-1 functions
+const validate_form_1 = () => {
+  const isName = is_valid_input(name_input, name_helpers);
+  const isEmail = is_valid_input(email_input, email_helpers);
+  const isPhone = is_valid_input(phone_input, phone_helpers);
+
+  if (isName && isEmail && isPhone) {
+    set_active_nav(2);
+    show_form(1, 2);
+  }
+};
+
+const is_valid_input = (
+  input: HTMLInputElement,
+  helper: HTMLParagraphElement,
+): boolean => {
+  if (input.value === "") {
+    helper.textContent = FIELD_REQUIRED;
+    return false;
+  }
+  if (input.type === "email" && !validate_email(input.value)) {
+    helper.textContent = EMAIL_INVALID_FORMAT;
+    return false;
+  } else {
+    helper.textContent = "";
+    return true;
+  }
+};
+
+const validate_email = (email_string: string) => {
+  return String(email_string)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+};
+
+// form-2 function
+const show_yearly_msg = (isShow: boolean) => {
+  document.querySelectorAll(".plan-toggle")!.forEach(item => {
+    item.classList.remove("active-toggle");
+  });
+  if (isShow) {
+    document.querySelectorAll(".yearly-msg")!.forEach(item => {
+      item.textContent = PLAN_YEARLY;
+    });
+    document.querySelector(".plan-yearly")!.classList.add("active-toggle");
+  } else {
+    document.querySelectorAll(".yearly-msg")!.forEach(item => {
+      item.textContent = "";
+    });
+    document.querySelector(".plan-monthly")!.classList.add("active-toggle");
+  }
+};
+
+// form-3 function
+const show_active_checkbox = (isShow: boolean, num: number) => {
+  if (isShow) {
+    document.querySelector(`.addon-${num}`)!.classList.add("active-checkbox");
+  } else {
+    document
+      .querySelector(`.addon-${num}`)!
+      .classList.remove("active-checkbox");
+  }
+};
+
+const show_addon_price = (isYearlyPlan: boolean) => {
+  if (isYearlyPlan) {
+    document.querySelector(".addon-1 .addon-price")!.textContent = "+$10/yr";
+    document.querySelector(".addon-2 .addon-price")!.textContent = "+$20/yr";
+    document.querySelector(".addon-3 .addon-price")!.textContent = "+$20/yr";
+  } else {
+    document.querySelector(".addon-1 .addon-price")!.textContent = "+$1/mo";
+    document.querySelector(".addon-2 .addon-price")!.textContent = "+$2/mo";
+    document.querySelector(".addon-3 .addon-price")!.textContent = "+$2/mo";
+  }
+};
+
+//  Event listeners
+form_1_next.addEventListener("click", e => {
+  validate_form_1();
+});
+
+form_2_prev.addEventListener("click", e => {
+  set_active_nav(1);
+  show_form(2, 1);
+});
+
+form_2_next.addEventListener("click", e => {
+  set_active_nav(3);
+  show_form(2, 3);
+});
+
+form_3_prev.addEventListener("click", e => {
+  set_active_nav(2);
+  show_form(3, 2);
+});
+
+form_3_next.addEventListener("click", e => {
+  set_active_nav(4);
+  show_form(3, 4);
+});
+
+price_toggle.addEventListener("change", e => {
+  show_yearly_msg(price_toggle.checked);
+  show_addon_price(price_toggle.checked);
+});
+
+online_cb.addEventListener("change", e => {
+  show_active_checkbox(online_cb.checked, 1);
+});
+
+larger_cb.addEventListener("change", e => {
+  show_active_checkbox(larger_cb.checked, 2);
+});
+
+custom_cb.addEventListener("change", e => {
+  show_active_checkbox(custom_cb.checked, 3);
+});
