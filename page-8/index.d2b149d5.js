3e4e63cf81c1a8f7fa4509de377c8017ad54494b
firdaus <firdausthedev@gmail.com>
2023-02-26 18:25:39 +0800

    Updates

---
+++ page-8/index.d2b149d5.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("submit"),t=document.getElementById("email"),n=document.querySelector("form p");e.addEventListener("click",(e=>{e.preventDefault(),t.value?n.textContent="":n.textContent="Check your email please"}));
+//# sourceMappingURL=index.d2b149d5.js.map
---
+++ page-8/index.f83f6f12.js
@@ -0,0 +1,2 @@
+!function(){const e=document.getElementById("submit"),t=document.getElementById("email"),n=document.querySelector("form p");e.addEventListener("click",(e=>{e.preventDefault(),t.value?n.textContent="":n.textContent="Check your email please"}))}();
+//# sourceMappingURL=index.f83f6f12.js.map
